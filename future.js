db02148646d8fa9e25a03e429be555206c6c3815
HaQadosch Berraka <haqadosch@pm.me>
2019-07-28 21:59:45 +0100

    ch01 lvl1 sec12 understanding-basic-types

--- future.js
+++ future.js
@@ -1,2 +1,18 @@
-var threeSquared = Math.pow(3, 2)
-console.log(threeSquared)
+var threeSquared = Math.pow(3, 2);
+console.log(threeSquared);
+var OrderStatus;
+(function (OrderStatus) {
+    OrderStatus[OrderStatus["Paid"] = 1] = "Paid";
+    OrderStatus[OrderStatus["Shipped"] = 2] = "Shipped";
+    OrderStatus[OrderStatus["Completed"] = 3] = "Completed";
+    OrderStatus[OrderStatus["Cancelled"] = 4] = "Cancelled";
+})(OrderStatus || (OrderStatus = {}));
+var statis = OrderStatus.Shipped; //?
+var customer = {
+    name: "Lamps Ltd",
+    turnover: 200134,
+    active: true
+};
+customer.turnover = 5000;
+var numbers = [1, 3, 5];
+numbers.forEach(function (nbr) { return console.log(nbr); });
--- future.ts
+++ future.ts
@@ -1,2 +1,24 @@
 const threeSquared: number = 3 ** 2
 console.log(threeSquared)
+
+enum OrderStatus {
+  Paid = 1,
+  Shipped,
+  Completed,
+  Cancelled
+}
+
+let statis = OrderStatus.Shipped //?
+
+const customer = {
+  name: "Lamps Ltd",
+  turnover: 200134,
+  active: true
+}
+
+customer.turnover = 5000
+
+const numbers = [1, 3, 5]
+
+numbers.forEach(nbr => console.log(nbr))
+
