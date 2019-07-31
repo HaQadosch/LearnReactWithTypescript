21b9f7ee034f50d468b6e747663e83ab0b972ef5
Xavier Brinon <xavier.brinon@ecs.co.uk>
2019-07-31 14:25:14 +0100

    ch04 lvl1 sec44 handling-not-found-routes

---
+++ twinkle-shop/src/components/NotFoundPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const NotFoundPage: React.FC = () => {
+  return (
+    <span className='page-container'>
+      <h2>Sorry, this page cannot be found</h2>
+    </span>
+  )
+}
--- twinkle-shop/src/components/Routes.tsx
+++ twinkle-shop/src/components/Routes.tsx
@@ -1,18 +1,22 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { Header } from './Header'
 import { AdminPage } from './AdminPage'
 import { ProductPage } from './ProductPage'
 import { ProductDetailPage } from './ProductDetailPage'
+import { NotFoundPage } from './NotFoundPage'
 
 export const Routes: React.FC = () => {
   return (
     <Router>
       <span>
         <Header />
-        <Route path='/products' component={ProductPage} />
-        <Route path='/product/:id' component={ProductDetailPage} />
-        <Route path='/admin' component={AdminPage} />
+        <Switch>
+          <Route path='/products' component={ProductPage} />
+          <Route path='/product/:id' component={ProductDetailPage} />
+          <Route path='/admin' component={AdminPage} />
+          <Route component={NotFoundPage} />
+        </Switch>
       </span>
     </Router>
   )
