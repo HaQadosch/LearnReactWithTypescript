39e0551df6a0ddcd9974d887572e98307bf72fb1
Xavier Brinon <xavier.brinon@ecs.co.uk>
2019-07-31 14:36:29 +0100

    ch04 lvl1 sec45 implementing-page-redirects

---
+++ twinkle-shop/src/components/LoginPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const LoginPage: React.FC = () => {
+  return (
+    <span className='page-container'>
+      <h2>Login</h2>
+      <p>You need to Login...</p>
+    </span>
+  )
+}
--- twinkle-shop/src/components/Routes.tsx
+++ twinkle-shop/src/components/Routes.tsx
@@ -1,23 +1,26 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import React, { useState } from 'react'
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 import { Header } from './Header'
 import { AdminPage } from './AdminPage'
 import { ProductPage } from './ProductPage'
 import { ProductDetailPage } from './ProductDetailPage'
 import { NotFoundPage } from './NotFoundPage'
+import { LoginPage } from './LoginPage'
 
 export const Routes: React.FC = () => {
+  const [loggedIn/*, setLoggedIn*/] = useState<boolean>(false)
+
   return (
     <Router>
-      <span>
-        <Header />
-        <Switch>
-          <Route path='/products' component={ProductPage} />
-          <Route path='/product/:id' component={ProductDetailPage} />
-          <Route path='/admin' component={AdminPage} />
-          <Route component={NotFoundPage} />
-        </Switch>
-      </span>
-    </Router>
+      <Header />
+      <Switch>
+        <Redirect exact={true} from='/' to='/products' />
+        <Route path='/products' component={ProductPage} />
+        <Route path='/product/:id' component={ProductDetailPage} />
+        <Route path='/admin'>{loggedIn ? <AdminPage /> : <Redirect to='/login' />}</Route>
+        <Route path='/login' component={LoginPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </Router >
   )
 }
