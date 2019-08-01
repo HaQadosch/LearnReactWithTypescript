c12bff34a7248693c9ceea7e12aec83129e2f1ca
Xavier Brinon <xavier.brinon@ecs.co.uk>
2019-08-01 09:08:32 +0100

    ch04 lvl1 sec49 animated-transitions

--- twinkle-shop/src/components/Routes.tsx
+++ twinkle-shop/src/components/Routes.tsx
@@ -1,26 +1,38 @@
 import React, { useState } from 'react'
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
+import './Routes.css'
+import { BrowserRouter as Router, Route, Switch, Redirect, RouteComponentProps } from 'react-router-dom'
 import { Header } from './Header'
 import { AdminPage } from './AdminPage'
 import { ProductPage } from './ProductPage'
 import { ProductDetailPage } from './ProductDetailPage'
 import { NotFoundPage } from './NotFoundPage'
 import { LoginPage } from './LoginPage'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 
-export const Routes: React.FC = () => {
+const Routes_: React.FC<RouteComponentProps> = ({ location: { key } }) => {
   const [loggedIn/*, setLoggedIn*/] = useState<boolean>(true)
 
   return (
-    <Router>
+    <section>
       <Header />
-      <Switch>
-        <Redirect exact={true} from='/' to='/products' />
-        <Route path='/products' component={ProductPage} />
-        <Route path='/product/:id' component={ProductDetailPage} />
-        <Route path='/admin'>{loggedIn ? <AdminPage /> : <Redirect to='/login' />}</Route>
-        <Route path='/login' component={LoginPage} />
-        <Route component={NotFoundPage} />
-      </Switch>
-    </Router >
+      <TransitionGroup>
+        <CSSTransition
+          key={key}
+          timeout={500}
+          classNames='animate' >
+          <Switch>
+            <Redirect exact={true} from='/' to='/products' />
+            <Route path='/products' component={ProductPage} />
+            <Route path='/product/:id' component={ProductDetailPage} />
+            <Route path='/admin'>{loggedIn ? <AdminPage /> : <Redirect to='/login' />}</Route>
+            <Route path='/login' component={LoginPage} />
+            <Route component={NotFoundPage} />
+          </Switch>
+        </CSSTransition>
+      </TransitionGroup>
+    </section >
   )
 }
+
+// A wrapper just to insert the History into Routes_
+export const Routes: React.FC = () => <Router><Route component={Routes_} /></Router>
