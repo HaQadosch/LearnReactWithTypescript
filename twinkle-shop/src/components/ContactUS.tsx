124799d84d32773c307e64a8e279ba7e3798453f
HaQadosch Berraka <haqadosch@pm.me>
2019-08-20 20:54:20 +0100

    ch07 lvl1 sec74 validating-forms

--- twinkle-shop/src/components/ContactUS.tsx
+++ twinkle-shop/src/components/ContactUS.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import './ContactUS.css'
-import { Form, FormField } from './Form';
+import { Form, FormField, required, minLength } from './Form';
 
 export const ContactUS: React.FC = () => {
   return (
-    <Form defaultValues={{ name: '', email: '', reason: 'Support', notes: '' }}>
+    <Form
+      defaultValues={{ name: '', email: '', reason: 'Support', notes: '' }}
+      validationRules={{
+        email: { validator: required },
+        name: [{ validator: required }, { validator: minLength, arg: 2 }]
+      }}
+    >
       <FormField name='name' label='Your name' />
       <FormField name='email' label='Your email' type='Email' />
       <FormField name='reason' label='Reason you need to contact us' type='Select' options={['Marketing', 'Support', 'Feedback', 'Jobs', 'Offer']} />
--- twinkle-shop/src/components/Form.tsx
+++ twinkle-shop/src/components/Form.tsx
@@ -1,13 +1,17 @@
-import React, { useState, createContext } from 'react'
+import React, { useState, createContext, useEffect } from 'react'
 import produce from 'immer'
+import './Form.css'
 
+interface IErrors {
+  [key: string]: string[]
+}
 interface IValues {
   [key: string]: any
-
 }
 
 interface IFormProps {
   defaultValues: IValues
+  validationRules: IValidationProps
 }
 
 interface IFormFieldProps {
@@ -20,27 +24,66 @@ interface IFormFieldProps {
 interface IFormContext {
   values: IValues
   setValue?: (fieldname: string, value: any) => void
+  errors: IErrors
+  validate?: (fieldname: string, value: any) => void
 }
 
-const FormContext = createContext<IFormContext>({ values: {} })
+const FormContext = createContext<IFormContext>({ values: {}, errors: {} })
+
+export type Validator = (fieldname: string, values: IValues, args?: any) => string
+
+export const required: Validator = (fieldName, values, args): string => {
+  let validate = ''
+  if (values[fieldName] === undefined || values[fieldName] === null || values[fieldName] === '') {
+    validate = 'This must be filled in.'
+  }
+  return validate
+}
+
+export const minLength: Validator = (fieldname, values, length: number): string => {
+  let validate = ''
+  if (values[fieldname] && values[fieldname].lenght < length) {
+    validate = `This must be at least ${length} character long.`
+  }
+  return validate
+}
+
+interface IValidation {
+  validator: Validator
+  arg?: any
+}
+
+interface IValidationProps {
+  [key: string]: IValidation | IValidation[]
+}
 
 export const FormField: React.FC<IFormFieldProps> = ({ name, label, type = 'Text', options }) => {
   return (
     <FormContext.Consumer>
-      {({ values, setValue }) => {
+      {({ values, setValue, validate, errors }) => {
         const handleChange: React.ChangeEventHandler<HTMLTextAreaElement | HTMLSelectElement | HTMLInputElement> = ({ currentTarget: { value: targetValue } }) => {
           if (setValue) {
             setValue(name, targetValue)
           }
         }
+
+        const handleBlur: React.FocusEventHandler<HTMLTextAreaElement | HTMLSelectElement | HTMLInputElement> = ({ currentTarget: { value: targetValue } }) => {
+          if (validate) {
+            validate(name, targetValue)
+          }
+        }
+
         return <fieldset className='form-group'>
           <label htmlFor={name}>{label}</label>
-          {(type === 'Text' || type === 'Email') && <input type={type.toLowerCase()} name={name} id={name} value={values[name]} onChange={handleChange} />}
-          {(type === 'TextArea') && <textarea id={name} value={values[name]} onChange={handleChange} />}
+          {(type === 'Text' || type === 'Email') && <input type={type.toLowerCase()} name={name} id={name} value={values[name]} onChange={handleChange} onBlur={handleBlur} />}
+          {(type === 'TextArea') && <textarea id={name} value={values[name]} onChange={handleChange} onBlur={handleBlur} />}
           {(type === 'Select') && (
-            <select name={name} id={name} value={values[name]} onChange={handleChange} >
+            <select name={name} id={name} value={values[name]} onChange={handleChange} onBlur={handleBlur} >
               {options && options.map((opt, ind) => <option value={opt} key={ind} >{opt}</option>)}
             </select>
+          )}
+          {errors[name] && errors[name].length > 0 && errors[name].map(
+            error => <span key={error} className='form-error'>{error}</span>
           )}
         </fieldset>
       }
@@ -49,8 +92,18 @@ export const FormField: React.FC<IFormFieldProps> = ({ name, label, type = 'Text
   )
 }
 
-export const Form: React.FC<IFormProps> = ({ defaultValues, children }) => {
+export const Form: React.FC<IFormProps> = ({ defaultValues, validationRules, children }) => {
   const [values, setValues] = useState<IValues>(defaultValues)
+  const [errors, setErrors] = useState<IErrors>({})
+
+  useEffect(() => {
+    const errors: IErrors = {}
+    Object.keys(defaultValues).forEach(fieldName => {
+      errors[fieldName] = []
+    })
+    setErrors(errors)
+    // eslint-disable-next-line
+  }, [])
 
   const setNewValues = (fieldname: string, value: any) => {
     setValues(produce(values, draft => {
@@ -62,7 +115,29 @@ export const Form: React.FC<IFormProps> = ({ defaultValues, children }) => {
     evt.preventDefault()
   }
 
-  const context: IFormContext = { values, setValue: setNewValues }
+  const validate = (fieldName: string, value: any): string[] => {
+    const rules = validationRules[fieldName]
+    const newErrors: string[] = []
+    if (Array.isArray(rules)) {
+      rules.forEach(({ validator, arg }) => {
+        const error = validator(fieldName, values, arg)
+        if (error) {
+          newErrors.push(error)
+        }
+      })
+    } else if (rules) {
+      const error = rules.validator(fieldName, values, rules.arg)
+      if (error) {
+        newErrors.push(error)
+      }
+    }
+    setErrors(produce(errors, draft => {
+      draft[fieldName] = newErrors
+    }))
+    return newErrors
+  }
+
+  const context: IFormContext = { values, setValue: setNewValues, errors, validate }
 
   return (
     <FormContext.Provider value={context}>
