ced4785ca6f43b958959b509c362855d5edd0a74
haqadosch <haqadosch@gmail.com>
2019-09-20 12:00:39 +0100

    ch09 lvl1 sec93, Canceling requests

--- crud-api/src/App.tsx
+++ crud-api/src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
-import axios from 'axios'
+import axios, { CancelTokenSource } from 'axios'
 
 interface IPost {
   userId: number
@@ -17,37 +17,57 @@ interface IPosts {
 const App: React.FC = () => {
   const [posts, setPosts] = useState<IPost[]>([])
   const [error, setError] = useState<string>('')
+  const [loading, setLoading] = useState<boolean>(false)
+  const [cancelTokenSource/*, setCancelTokenSource*/] = useState<CancelTokenSource>(axios.CancelToken.source())
+
+  const handleCancelClick: React.MouseEventHandler = () => {
+    if (cancelTokenSource) {
+      console.log('User canceled operation')
+      cancelTokenSource.cancel('User canceled operation')
+    }
+  }
 
   useEffect(() => {
     async function getSome() {
+      setLoading(true)
       axios
         .get<IPost[]>('https://jsonplaceholder.typicode.com/posts', {
+          cancelToken: cancelTokenSource.token,
           headers: {
             'Content-Type': 'application/json'
           },
           timeout: 5000
         })
         .then(({ data }) => {
+          setLoading(false)
           setPosts(data)
         })
-        .catch(({ response: { status = 0 } = { status: 0 }, code }) => {
-          console.log({ code })
-          const error = code === 'ECONNABORTED' 
-            ? 'a timeout occured' 
-            : status === 404
-              ? 'resource not found'
-              : 'unexpected error occured'
+        .catch(ex => {
+          setLoading(false)
+          const { response: { status = 0 } = { status: 0 }, code = '' } = ex
+          const error = axios.isCancel(ex)
+            ? 'request canceled'
+            : code === 'ECONNABORTED'
+              ? 'a timeout occured'
+              : status === 404
+                ? 'resource not found'
+                : 'unexpected error occured'
           setError(error)
         })
     }
 
     getSome()
+    // eslint-disable-next-line
   }, [])
 
   return (
     <div className="App">
       {error !== ''
         ? <p className='error'>{error}</p>
+        : null
+      }
+      {loading
+        ? <button onClick={handleCancelClick}>Cancel</button>
         : null
       }
       <ul className="posts">{
