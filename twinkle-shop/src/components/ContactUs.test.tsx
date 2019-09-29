1861b83b994fac554dc52cfd844d47c14bbb3038
HaQadosch Berraka <haqadosch@pm.me>
2019-09-29 12:42:29 +0100

    ch11 lvl1 sec109, using jest snapshot tests

--- twinkle-shop/src/components/ContactUs.test.tsx
+++ twinkle-shop/src/components/ContactUs.test.tsx
@@ -13,27 +13,32 @@ const handleSubmit = jest.fn<Promise<ISubmitResult>, any[]>(async () => {
 afterEach(cleanup)
 
 describe('<ContactUS />', () => {
+  it('should renders ok', () => {
+    const { container } = render(<ContactUS onSubmit={handleSubmit} />)
+    expect(container).toMatchSnapshot()
+  });
+
   it('should display 2 error messages when submitted empty.', () => {
-    const { getByText, getAllByText } = render(<ContactUS onSubmit={handleSubmit} />)
+    const { getByText, getAllByText } = render(<ContactUS onSubmit={ handleSubmit } />)
 
     const submitBtn = getByText('Submit')
 
     fireEvent.click(submitBtn)
 
     expect(handleSubmit).toHaveBeenCalledTimes(1)
-    expect(handleSubmit).toHaveBeenCalledWith({"email": "", "name": "", "notes": "", "reason": "Support"})
+    expect(handleSubmit).toHaveBeenCalledWith({ "email": "", "name": "", "notes": "", "reason": "Support" })
     expect(getAllByText('This must be filled in.')).toHaveLength(2)
   });
-  
+
   it('should submit okay if filled in properly', () => {
-    const { getByText, getByLabelText, queryByText, debug } = render(<ContactUS onSubmit={handleSubmit} />)
+    const { getByText, getByLabelText, queryByText, debug } = render(<ContactUS onSubmit={ handleSubmit } />)
 
     const nameInput = getByLabelText('Your name')
     const emailInput = getByLabelText('Your email')
     const submitBtn = getByText('Submit')
 
-    fireEvent.change(nameInput, { target: { value: 'Carl'}})
-    fireEvent.change(emailInput, { target: { value: 'carl@rippon.eu'}})
+    fireEvent.change(nameInput, { target: { value: 'Carl' } })
+    fireEvent.change(emailInput, { target: { value: 'carl@rippon.eu' } })
     fireEvent.click(submitBtn)
 
     expect(queryByText('This must be filled in.')).not.toBeInTheDocument()
